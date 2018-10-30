ce4cb51c79ed10187cf09881ab0792a3d30a20f0
Jindrich Kvita <jindrich.kvita@gnj.cz>
2018-10-30 17:49:11 +0100

    zacatek cviceni 7

---
+++ cv7/lab07/src/pl1/lab07/Main01.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Jan Kožusznik
+ * Copyright (c) 2016 All Right Reserved, http://www.kozusznik.cz
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ ******************************************************************************/
+package pl1.lab07;
+
+import pl1.shapes.manager.CanvasManager;
+
+/**
+ * @author Jan Kožusznik
+ * @version 0.1
+ */
+public class Main01 {
+
+  public static void main(String[] args) {
+    CanvasManager.getInstance();
+
+  }
+
+}
